a805c0648b35943a4108358cde066d22f9b545f0
Aaron Meese <ajmeese7@gmail.com>
2019-02-03 20:36:47 -0600

    Initial file upload
    App is currently defunct, but I want to go ahead and start the repo to motivate me to make progress.

---
+++ lib/SQLmanipulation.dart
@@ -0,0 +1,12 @@
+import 'package:sqflite/sqflite.dart';
+
+/* IDEA:
+   Use the same style PHP "API" that I used for Jessica
+   to access the GoDaddy programatically-populated
+   tables and modify data? Or determine an alternative
+   method of accessing SQL data, because sqflite doesn't
+   seem like it's going to cut it (example code giving errors)
+*/
+
+var databasesPath = await getDatabasesPath();
+String path = join(databasesPath, 'demo.db');
