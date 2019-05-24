3e8a60a285975aefc4885b0e1a4015ee5065187e
Aaron Meese <ajmeese7@gmail.com>
2019-05-24 13:32:47 -0500

    Working login
    Started updating mobile menu, have a semi-functioning log in and log out, and added TOS and privacy policy that will need further development.

---
+++ javascript/firebase.js
@@ -0,0 +1,82 @@
+// Initialize Firebase
+var config = {
+    // TODO: How to obsfucate this?
+    apiKey: "AIzaSyAlXDmH2X2zZ1vUhP3TQ2AZ0yQVutiDQGM",
+    authDomain: "coupon-booked.firebaseapp.com",
+    databaseURL: "https://coupon-booked.firebaseio.com",
+    projectId: "coupon-booked",
+    storageBucket: "coupon-booked.appspot.com",
+    messagingSenderId: "243041825180"
+};
+firebase.initializeApp(config);
+
+var uiConfig = {
+    // IDEA: Could send them to their profile for the redirect, but be careful, because that was causing problems last time.
+    // Ties into ui.start() method linked
+    signInSuccessUrl: 'http://couponbooked.com',
+    signInOptions: [
+        // TODO: Enable all sign-in options and configure
+        // IDEA: Instead of guest login, ust add option to send coupon book anonymously
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Is the callback function better or different in any way?
+    tosUrl: 'tos.html',
+    privacyPolicyUrl: function() {
+        window.location.assign('privacy.html');
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+// Is pending redirect option needed? https://github.com/firebase/firebaseui-web#using-firebaseui-for-authentication
+
+
+// TODO: Use this tracking state code to determine whether each page's nav needs
+// to show the sign in button or the user's information on the fancy menu.
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getIdToken().then(function(accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                // TODO: Need to change this to show user image that leads to fancy drop down menu
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, '  ');
+            });
+        } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'User not logged in';
+        }
+    }, function(error) {
+        console.error(error);
+    });
+};
+
+window.addEventListener('load', function() {
+    initApp();
+});
--- javascript/index.js
+++ javascript/index.js
@@ -19,21 +19,40 @@ $(function() {
     });
 });
 
-// Handle mobile menu
-var exitMenuButton = "<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times; Coupon Booked</a>";
-var openMenuButton = "<span style='font-size: 30px; cursor: pointer' onclick='openNav()'><img id='logo' src='images/logo.png'></span>";
-function openNav() {
-    // TODO: Find safer alternative to innerHTML if necessary (BELOW as well!)
-    document.getElementById("main").innerHTML = exitMenuButton;
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementsByTagName("main")[0].style.marginLeft = "250px";
-    //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    // NOTE: This method of transparency doesn't work. TODO: Use a div.
-}
+// This is a general function that removes one class and adds another
+function toggleClass(targetElement, addedClass) {
+    if (targetElement.classList.contains(addedClass)) {
+        targetElement.classList.remove(addedClass);
+    } else {
+        targetElement.classList.add(addedClass);
+    }
+};
 
-function closeNav() {
-    document.getElementById("main").innerHTML = openMenuButton;
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementsByTagName("main")[0].style.marginLeft= "0";
-    //document.body.style.backgroundColor = "white";
-}
+// This is the function we add to our menu button to enable its click functionality
+// TODO: Add some kind of toggle associated with this to change the image from the logo to an X.
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    toggleClass(document.querySelector('.menu'), 'menu--open');
+});
+
+document.querySelector('#sign-in').addEventListener('click', function() {
+    if (document.querySelector('#sign-in').innerText == "Sign in") {
+        // TODO: Make the whole transition from main page to sign in smoother
+        $("#firebaseui-auth-container").fadeIn(1000);
+        //toggleClass(document.querySelector('#firebaseui-auth-container'), 'hide');
+
+        // IDEA: Make it accept an array of selectors as well? Avoids these four calls by combining
+        toggleClass(document.querySelector('nav'), 'hide');
+        toggleClass(document.querySelector('main'), 'hide');
+        toggleClass(document.querySelector('footer'), 'hide');
+    } else if (document.querySelector('#sign-in').innerText == "Sign out") {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            alert("Signed out successfully.");
+        }).catch(function(error) {
+            // An error happened.
+            console.error(error);
+        });
+    }
+    // IDEA: Toggle to an x in case the user does not want to sign in. (&times;)
+    // Or instead of 'toggling' to an x, hide it and use an x button that works that way.
+});
