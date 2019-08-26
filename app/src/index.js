06d6508432b54a1d566a7902752c9f5c9bbea50d
Aaron Meese <ajmeese7@gmail.com>
2019-08-26 12:40:51 -0500

    Native back button support
    On Android, the back native back button has the same functionality as the programmed one. On pages without a back button, it currently does nothing, but that shouldn't be hard to implement in the future if I so choose.

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,8 @@ var Auth0Cordova =  require('@auth0/cordova');
 var App = require('./App');
 
 function main() {
+    document.addEventListener("backbutton", handleNativeBackButton, false);
+
     var app = new App();
     function intentHandler(url) {
         Auth0Cordova.onRedirectUri(url);
@@ -11,3 +13,16 @@ function main() {
 }
 
 document.addEventListener('deviceready', main);
+
+function handleNativeBackButton() {
+    // IDEA: Set this to go to previous route if no #backArrow;
+    // close app if precious route /login or /
+    var backButton = document.getElementById("backArrow");
+    if (backButton) {
+        console.warn("Native back button calling listener...");
+        $('#backArrow').click();
+    } else {
+        // NOTE: This prevents the default behavior of the back button
+        console.warn("Back button not supported on this page!");
+    }
+}
