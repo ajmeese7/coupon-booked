90c2cb8c06385aaa2344d53d53baf6a913bc182a
Aaron Meese <ajmeese7@gmail.com>
2019-09-04 16:40:58 -0500

    Support for OneSignal notifications
    This is eventually going to be used for redeeming coupons, but for now it only supports notifications sent from the OneSignal dashboard. The Android folder is explained in the README changes.

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,11 @@
 var Auth0Cordova =  require('@auth0/cordova');
 var App = require('./App');
+const env = require('../env');
 
+document.addEventListener('deviceready', main);
 function main() {
     document.addEventListener("backbutton", handleNativeBackButton, false);
+    onesignalNotifications();
 
     var app = new App();
     function intentHandler(url) {
@@ -12,11 +15,9 @@ function main() {
     app.run('#app');
 }
 
-document.addEventListener('deviceready', main);
-
 function handleNativeBackButton() {
     // IDEA: Set this to go to previous route if no #backArrow;
-    // close app if precious route /login or /
+    // close app if previous route /login or /
     var backButton = document.getElementById("backArrow");
     if (backButton) {
         console.warn("Native back button calling listener...");
@@ -25,4 +26,21 @@ function handleNativeBackButton() {
         // NOTE: This prevents the default behavior of the back button
         console.warn("Back button not supported on this page!");
     }
+}
+
+// TODO: Set all of this up for iOS as well once testing commences
+function onesignalNotifications() {
+    // Enable to debug issues.
+    // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+    
+    var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback:');
+        console.log(jsonData);
+    };
+    
+    // TODO: Prevent it from displaying alert if currently in app?
+    window.plugins.OneSignal
+        .startInit(env.ONESIGNAL_ID)
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
 }
